48660b27b5f8566b9af6f5a280665e6908304e2f
AO-Alumno <AO-Alumno@duoc.acad>
2026-04-22 19:48:06 -0400

    funciones admin para crear

--- src/main/java/com/siscontrol/backend/controllers/UserController.java
+++ src/main/java/com/siscontrol/backend/controllers/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import com.siscontrol.backend.dto.CreateUserRequestDTO;
 import com.siscontrol.backend.dto.UserResponseDTO;
-import com.siscontrol.backend.models.User;
 import com.siscontrol.backend.services.UserService;
 
 @RestController
